74c0b3e89d98b4984d7375ff28b8fbcbf51757ca
Clayton Miller <clayton.miller@loop.colum.edu>
2021-02-01 20:50:59 -0600

    adds cors

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const cors = require("cors");
+const corsOptions = require("./configs/cors.js");
+
 const express = require('express')
 const app = express()
 // const PORT = process.env.PORT || 4000
 
 
 // Add the middleware code needed to accept incoming data and add it to req.body
+// NODE_ENV === "production" ? app.use(cors(corsOptions)) : 
+app.use(cors())
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
@@ -12,7 +17,6 @@ app.use('/api/cookbooks/', cookbookRouter)
 
 const authorRouter = require('./controllers/authorRoutes')
 app.use('/api/authors/', authorRouter)
-
 
 
 
